fdf8affdd493a712fa77bb182c3a2937bc6399da
MathisGD <delaunay.mathis@hotmail.fr>
2021-12-13 12:34:22 +0100

    reeantrancy: switch to uint mapping

--- contracts/Vulnerable.sol
+++ contracts/Vulnerable.sol
@@ -2,16 +2,24 @@
 pragma solidity 0.8.7;
 
 contract Vulnerable {
-    bool private hasBeenWithdrawed = false;
+    mapping(address => uint256) balances;
+
+    constructor() {
+        balances[0xfB281ce015C885fB5096907a59c80c391B2599E9] = 1 ether; // NonAttackerContract
+        balances[0x197fD5E20C4073683C551416685AFA6e3E4cE1Aa] = 1 ether; // AttackerContract
+    }
 
     receive() external payable {}
 
     function withdraw() public {
-        require(!hasBeenWithdrawed, "Your ether has already been withdrawed");
+        require(
+            balances[msg.sender] != 0,
+            "Your ether has already been withdrawed"
+        );
 
-        (bool success, ) = msg.sender.call{value: 1 ether}("");
+        (bool success, ) = msg.sender.call{value: balances[msg.sender]}("");
         require(success, "Transaction failed");
 
-        hasBeenWithdrawed = true;
+        balances[msg.sender] = 0;
     }
 }
